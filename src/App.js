2250bc33257215f9ad6fe39e68bedcdd143e129f
Timothée Diot <timotheediot@gmail.com>
2019-10-08 20:42:02 +0200

    resolve App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact 
+        name= "John Doe" 
+        avatar= "https://randomuser.me/api/portraits/thumb/men/75.jpg" 
+        online= {true}
+        />
+        <Contact 
+        name= "Sophie Lafutte" 
+        avatar= "https://randomuser.me/api/portraits/thumb/men/75.jpg" 
+        online= {false}
+        />
+        <Contact 
+        name= "Agathe Gnagna" 
+        avatar= "https://randomuser.me/api/portraits/thumb/men/75.jpg" 
+        online= {true}
+        />
     </div>
   );
 }
