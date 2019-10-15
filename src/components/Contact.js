4a913f622aeca6a921060ac6c9c301a6902e5802
Tim <Tim@MacBook-Pro-de-Tim.local>
2019-10-15 10:53:54 +0200

    add contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,16 +3,19 @@ import './Contact.css'
 
 
 class Contact extends React.Component {
+    state = {
+        online: false
+    }
+
     render() {
     return (
         <div>
             <figure className="Contact">
                 <img className="avatar"
-                src={this.props.avatar} />
+                src={this.props.avatar} /> 
                 <div>
                     <cite>{this.props.name}</cite>
                     <div className="status">
-                        
                         <div className="status-text">
                         <div className={this.props.online ? "status-online" : "status-offline"}></div>
                         {this.props.online ? 'Online' : 'Offline'}
